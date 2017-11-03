2296f81763c746afb19586073a1f25b613147215
Matt Elia <matt@Matts-Mac-mini.fios-router.home>
2017-11-02 22:46:22 -0400

    init - react-native + nodejs backend -- google signin

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const google = {
+  clientID: '501147825032-1qgfdq4ggdtr0q1m60c5138q7phqbsh8.apps.googleusercontent.com',
+  clientSecret: 'LDHZch4ng4di43BKDJUdoe5a',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+}
